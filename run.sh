f9a93e9db1299a7047d33e7ec1564c184334c27c
KillByte <killbyte.prototype@gmail.com>
2020-11-02 15:41:02 +0000

    + run.sh create env dir if not exists

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ ! -d "discord_bot_env/" ]; then
+    python3 -m venv discord_bot_env
+    source discord_bot_env/bin/activate
+    pip install -r requirements.txt
+fi
+
 [ ! -f "src/auth.py" ] && echo -e "# Discord Authentication TOKEN\nTOKEN=None" > src/auth.py
+
 source discord_bot_env/bin/activate
 python3 src/bot.py
